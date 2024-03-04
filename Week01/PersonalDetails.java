86d35c559aa9a66b209be0519550abc774490e80
Prasijosi <jprashiddha5@gmail.com>
2024-03-04 11:26:15 +0545

    WEEK 01

---
+++ Week01/PersonalDetails.java
@@ -0,0 +1,16 @@
+
+public class PersonalDetails {
+    public static void main(String args[]) {
+
+        String Name = "Prashiddha Joshi";
+        int age = 19;
+        int Phone = 9818116528;
+        String address = "Chapagaun";
+
+        System.out.println(Name);
+        System.out.println(age);
+        System.out.println(Phone);
+        System.out.println(address);
+
+    }
+}
